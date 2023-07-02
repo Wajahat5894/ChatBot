cfdf4937ff399d8cdb9b959ce2d9c8b0000fc0fb
Sheeza Kanwal <sheezakanwal@mbp-von-sheeza.speedport.ip>
2023-07-02 11:39:02 +0200

    Aktuell

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# ChatBot
+# Chatbot mit Spracherkennung und -synthese
+
+Dieses Projekt ist ein einfacher Chatbot, der Spracheingaben über ein Mikrofon entgegennehmen, diese in Text umwandeln, den Text verarbeiten, eine Antwort generieren und diese Antwort dann synthetisieren und aussprechen kann.
+
+## Setup
+
+### Umgebungsvariablen
+
+Stellen Sie sicher, dass die folgenden Umgebungsvariablen gesetzt sind:
+
+- AWS_ACCESS_KEY_ID: Ihr AWS Access Key.
+- AWS_SECRET_ACCESS_KEY: Ihr AWS Secret Access Key.
+- AWS_REGION: Die AWS-Region, in der Ihre S3-Buckets und Transcribe-Dienste gehostet werden.
+- OPENAI_API_KEY: Ihr API-Schlüssel für OpenAI.
+
+### Installation
+
+1. Klone dieses Repository auf Ihren lokalen Computer:
+
+
+2. Navigieren Sie in das Verzeichnis des Projekts:
+
+
+3. Installieren Sie die erforderlichen Pakete:
+   
+   pip install -r requirements.txt
+
+
+## Verwendung
+
+Um den Chatbot zu starten, führen Sie das Hauptscript mit Python 3 aus:
+
+python3 main.py
+
+
+Der Chatbot wartet dann auf Spracheingaben, transkribiert diese in Text, verarbeitet den Text, generiert eine Antwort und spricht die Antwort aus.
+
+
+
--- speech.py
+++ speech.py
@@ -1,36 +1,60 @@
-# Anpassungen in speech.py
 import boto3
 import os
 import pygame
-from pydub import AudioSegment
-from pydub.silence import detect_nonsilent
+import io
+import soundfile as sf
 import numpy as np
 import sounddevice as sd
+from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
+import openai
 
 polly_client = boto3.Session(
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
     region_name='eu-central-1').client('polly')
 
+
+def numpy_to_audio_segment(audio_numpy, frame_rate):
+    byte_io = io.BytesIO()
+    sf.write(byte_io, audio_numpy, frame_rate, format='wav')
+    byte_io.seek(0)
+    audio_segment = AudioSegment.from_file(byte_io, format="wav")
+    return audio_segment
+
+
 def get_voice_command():
-    # Anfang der Aufnahme
     print("Sprechen Sie jetzt...")
-    duration = 10  # vorläufige Dauer
-    fs = 44100  # Sample-Rate
+    duration = 10
+    fs = 44100
     my_recording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
-    sd.wait()  # Warten, bis die Aufnahme abgeschlossen ist
-    nonsilent = detect_nonsilent(AudioSegment(my_recording, frame_rate=fs))
-    # Wenn die Sprache aufhört, die Aufnahme stoppen
+    sd.wait()
+    my_audio_segment = numpy_to_audio_segment(my_recording, fs)
+    nonsilent = detect_nonsilent(my_audio_segment)
+
     if nonsilent:
         end_time = nonsilent[-1][1]
-        my_recording = my_recording[:end_time*fs]
-    # Verwenden Sie Amazon Transcribe, um die Sprache zu transkribieren
+        my_recording = my_recording[:int(end_time*fs)]
+
     transcribe_client = boto3.client('transcribe', region_name='eu-central-1')
+    s3_bucket_name = 'dein-gueltiger-bucket-name'
+    s3_client = boto3.client('s3', region_name='eu-central-1')
+
+    byte_io = io.BytesIO()
+    sf.write(byte_io, my_recording, fs, format='wav')
+    byte_io.seek(0)
+    s3_client.upload_fileobj(byte_io, s3_bucket_name, 'my-recording.wav')
+
+    s3_uri = f's3://{s3_bucket_name}/my-recording.wav'
+
     response = transcribe_client.start_transcription_job(
         TranscriptionJobName='MyTranscriptionJob',
+        Media={'MediaFileUri': s3_uri},
+        MediaFormat='wav',
         LanguageCode='de-DE',
-        Media={'MediaFileUri': 's3://my-bucket/my-recording.wav'}
+        OutputBucketName=s3_bucket_name
     )
+
     while True:
         status = transcribe_client.get_transcription_job(TranscriptionJobName='MyTranscriptionJob')
         if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
@@ -41,6 +65,7 @@ def get_voice_command():
         transcript_text = ''
     print(f"Sie sagten: {transcript_text}\n")
     return transcript_text
+
 
 def speak(text):
     response = polly_client.synthesize_speech(VoiceId='Hans',
@@ -58,5 +83,17 @@ def speak(text):
         continue
 
     os.remove('speech.mp3')
+
+
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+
+def generate_email_body(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-004",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=300
+    )
 
 
